aa189b8893551db0658b135c7c7f788897197f15
songhyeongju <ardieyes@gmail.com>
2022-10-20 14:38:40 +0900

    가상화폐 매수, 매도 기능 추가

--- python-back/controller/trade.py
+++ python-back/controller/trade.py
@@ -4,33 +4,37 @@ load_dotenv()
 access_key = os.environ['ACCESS_KEY']
 secret_key = os.environ['SECRET_KEY']
 
-def buy_order(access, secret, market, price):
+def buy_order(accessKey, secretKey, marketCode, price):
 		from pyupbit.exchange_api import Upbit
 		try:
-			upbit = Upbit(access, secret)
-			upbit.buy_market_order(market, str(price))
+			upbit = Upbit(accessKey, secretKey)
+			upbit.buy_market_order(marketCode, price)
+			return None
 		except Exception as x:
 			print(x)
 			return None
 
-def sell_order(access, secret, market, volume):
+def sell_order(accessKey, secretKey, marketCode, volume):
 		from pyupbit.exchange_api import Upbit
 		try:
-			upbit = Upbit(access, secret)
-			upbit.sell_market_order(market, str(volume))
+			upbit = Upbit(accessKey, secretKey)
+			upbit.sell_market_order(marketCode, volume)
+			return  None
 		except Exception as x:
 			print(x)
 			return None
 
-def sell_order(access, secret, uuid):
+def cancel_order(accessKey, secretKey, uuid):
 		from pyupbit.exchange_api import Upbit
 		try:
-			upbit = Upbit(access, secret)
-			upbit.sell_market_ordder(str(uuid))
+			upbit = Upbit(accessKey, secretKey)
+			upbit.cancel_order(str(uuid))
+			return None
 		except Exception as x:
 			print(x)
 			return None
 
 
+# buy_order(access_key, secret_key, 'KRW-XRP', 5000)
 
-
+# sell_order(access_key, secret_key, 'KRW-XRP', '8')
