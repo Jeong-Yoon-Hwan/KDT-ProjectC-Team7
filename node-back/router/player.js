17b1e1eaac23d5918b014755e90b50bad93eba4e
songhyeongju <ardieyes@gmail.com>
2022-10-21 16:31:32 +0900

    동영상 토큰 인증 삭제

--- node-back/router/player.js
+++ node-back/router/player.js
@@ -3,6 +3,6 @@ import { playerLoad, videoFile } from '../controller/player.js';
 import { isAuth } from '../middleware/auth.js';
 
 const router = express.Router();
-router.get('/', isAuth, playerLoad); //  서버주소/player에 접속하면 영상이 바로 재생됨
-router.get('/video', isAuth, videoFile);
+router.get('/', playerLoad); //  서버주소/player에 접속하면 영상이 바로 재생됨
+router.get('/video', videoFile);
 export default router;
