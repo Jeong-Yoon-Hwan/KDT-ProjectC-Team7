11b2dc837449499aa06c31d702ff1fd4b60efce4
songhyeongju <ardieyes@gmail.com>
2022-10-21 16:33:05 +0900

    자동매매 기능 라우터 연결, 소스 배포용 추가

--- python-back/controller/botDown.py
+++ python-back/controller/botDown.py
@@ -1,6 +1,5 @@
-# 리플로 테스트, 자본금 10,000원, 매도 수량은 잔고에서 조회
-# 원화로만 거래 가능
-# def autobot (accessKey, secretKey, marketCode, capital):
+# 소스 배포용
+
 import datetime as dt
 start_time = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 end_time = (dt.datetime.now() + dt.timedelta(1)).strftime('%Y-%m-%d %H:%M:%S')
@@ -8,19 +7,15 @@ while True:
 		try:				
 				import time
 				import pyupbit
-				import os
 				import pandas as pd
-				from pprint import pprint
-				from dotenv import load_dotenv
+	
 				from pyupbit.exchange_api import Upbit
 				
-				load_dotenv()
-				access_key = os.environ['ACCESS_KEY']
-				secret_key = os.environ['SECRET_KEY']
+				access_key = '' # 업비트 엑세스 키 
+				secret_key = '' # 업비트 시크릿 키				
+				marketCode = 'KRW-XRP' # 마켓코드 적을 것.
+				startCapital = 100000 # 12000원 이상 입금해야 함.
 				upbit = Upbit(access_key, secret_key)	
-
-				marketCode = 'KRW-XRP'
-				capital = 100000
 				########################################################################
 				df = pyupbit.get_ohlcv(marketCode, 'day', 3)
 				df = df.astype(int)
@@ -32,9 +27,9 @@ while True:
 				avg_buy_price = balance_all.iat[1,2] # 코인 평균 매수 가격
 				owned_balance = balance_all.iat[1,1] ## 현재 보유 코인 수량
 				capital_value = round(float(avg_buy_price) * float(owned_balance)) # 코인 현재 가치(KRW) > 5000 일때 거래 가능
+			
 				
 				real_time_price = pyupbit.get_current_price(marketCode) ## 실시간 현재가격
-				print(real_time_price)
 				yesterday = df.iloc[-2]
 				today = df.iloc[-1]
 				start_time = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -46,19 +41,20 @@ while True:
 				today_open = today['open']
 				today_close = today['close']
 
-				# 조건 1 현재 변동폭이 어제 변동폭보다 커야함.
+				# 매수 조건 1 : 현재 변동폭이 어제 변동폭보다 커야함.
 				yesterday_volatility = (yesterday_high - yesterday_low) / 100 * 0.5
 				today_volatility = (today_high - today_low) / 100 * 0.5
 				condition_volatility = (today_volatility > yesterday_volatility)
 
-				# 조건 2 시초가 대비 2% 이상 상승할 때
+				# 매수 조건 2 : 시초가 대비 2% 이상 상승할 때
 				condition_two_per = (((today_close / today_open) - 1) > (2 / 100))
 				target_buy = (condition_volatility and condition_two_per) ## 매수 조건
+
+				# 매도 조건 : 매수가 대비 3% 오르거나, 3% 하락하면 전량 매도
 				condition_increase = int(float(avg_buy_price) * 1.03)
 				condition_decrease = int(float(avg_buy_price) - (float(avg_buy_price) * 0.03))
 				target_sell = ((condition_increase < real_time_price) or condition_decrease > real_time_price) ## 매도 조건
-				print('hhahaha')
-				# 
+				print(real_time_price)
 				if ((float(currentCapital)> 5000) and (target_buy == True)):
 						upbit.buy_market_order(marketCode, currentCapital) # 잔고 전부 매수 #
 				if ((float(capital_value))>5000 and (target_sell == True)):		
@@ -68,7 +64,7 @@ while True:
 				if ((float(currentCapital) < 5000) and (float(capital_value) < 5000)): 
 						break
 				## 초기 자본금 대비 12% 누적 수익이 났을 때 break
-				if ((float(currentCapital) + float(capital_value)) > float(capital) * 1.12):
+				if ((float(currentCapital) + float(capital_value)) > float(startCapital) * 1.12):
 						break
 				## 자동매매 시작 후, 24시간이 지나면 break
 				if (start_time > end_time):
@@ -77,5 +73,4 @@ while True:
 					print(x)
 
 					time.sleep(2)
-
 
