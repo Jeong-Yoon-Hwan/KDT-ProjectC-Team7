21bba2a3289592c8748e4929a3ef221eaeabbb7c
songhyeongju <ardieyes@gmail.com>
2022-10-24 14:18:45 +0900

    다운로드 기능 진행중

--- node-back/controller/download.js
+++ node-back/controller/download.js
@@ -1,6 +1,10 @@
 import path from 'path';
 const currentDir = path.resolve();
 
-export async function downloadOne(req, res) {
+export async function autobot(req, res) {
   return res.sendFile(currentDir + '/download/botDown.py');
+}
+
+export async function downPage(req, res) {
+  return res.sendFile(currentDir + '/client/download.html');
 }
--- node-back/router/download.js
+++ node-back/router/download.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { downloadOne } from '../controller/download.js';
+import { autobot, downPage } from '../controller/download.js';
 import { isAuth } from '../middleware/auth.js';
 
 const router = express.Router();
 
-router.get('/', downloadOne); // 접속하면 자동매매 파일을 받을 수 있게함.
+router.get('/', downPage);
+router.get('/autobot', autobot);
 
 export default router;
