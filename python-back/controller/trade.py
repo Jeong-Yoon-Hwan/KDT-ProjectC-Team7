4c5d2db61c031c78b22bd69540d5eec2f4aff6d8
songhyeongju <ardieyes@gmail.com>
2022-10-24 09:31:04 +0900

    매매기능 수정

--- python-back/controller/trade.py
+++ python-back/controller/trade.py
@@ -6,33 +6,21 @@ import os
 
 def buy_order(accessKey, secretKey, marketCode, price):
 		from pyupbit.exchange_api import Upbit
-		try:
-			upbit = Upbit(accessKey, secretKey)
-			upbit.buy_market_order(marketCode, price)
-			return None
-		except Exception as x:
-			print(x)
-			return None
+		upbit = Upbit(accessKey, secretKey)
+		upbit.buy_market_order(marketCode, price)
+		
 
 def sell_order(accessKey, secretKey, marketCode, volume):
 		from pyupbit.exchange_api import Upbit
-		try:
-			upbit = Upbit(accessKey, secretKey)
-			upbit.sell_market_order(marketCode, volume)
-			return  None
-		except Exception as x:
-			print(x)
-			return None
+		upbit = Upbit(accessKey, secretKey)
+		upbit.sell_market_order(marketCode, volume)
+			
 
 def cancel_order(accessKey, secretKey, uuid):
 		from pyupbit.exchange_api import Upbit
-		try:
-			upbit = Upbit(accessKey, secretKey)
-			upbit.cancel_order(str(uuid))
-			return None
-		except Exception as x:
-			print(x)
-			return None
+		upbit = Upbit(accessKey, secretKey)
+		upbit.cancel_order(str(uuid))
+
 
 
 # buy_order(access_key, secret_key, 'KRW-XRP', 5000)
